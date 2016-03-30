cb1a2fad7d22bfbeee53274675933aac999b3f73
Kevin Scheunemann <kscheunemann@athenahealth.com>
2016-03-30 14:50:15 -0400

    working devcloud image

--- scripts/cloud-init-conf.sh
+++ scripts/cloud-init-conf.sh
@@ -2,9 +2,16 @@ if [ -n "${RSYNC}" ]; then
     /usr/bin/rsync -a --delete rsync://cobbler100/athena/prod rsync://cobbler100/athena/techops rsync://cobbler100/athena/scripts rsync://cobbler100/athena/intranet rsync://cobbler100/athena/athenax /usr/local/athena
 fi
 
+mkdir -p /etc/athena/athenax
+echo development > /etc/athena/athenax/environment
+
 mkdir -p /etc/facter/facts.d
 echo "athena_server_role=${ROLE}" > /etc/facter/facts.d/athena.txt
 echo "build ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
+
+cat > /etc/cloud/cloud.cfg.d/01_timezone.cfg << EOM
+timezone: America/New_York
+EOM
 
 cat > /etc/cloud/cloud.cfg.d/03_puppet.cfg << EOM
 puppet:
@@ -14,12 +21,14 @@ puppet:
      certname: "%i.devcloud.athenahealth.com"
 EOM
 
-cat > /etc/cloud/cloud.cfg.d/09_copy_key.cfg << EOM
+cat > /etc/cloud/cloud.cfg.d/10_devcloud_setup.cfg << EOM
 runcmd:
  - "cat ~cloud-user/.ssh/authorized_keys >> ~build/.ssh/authorized_keys"
+ - "mkdir -p /etc/athena/athenax"
+ - "echo development > /etc/athena/athenax/environment"
+ - "/usr/bin/rsync -a --delete rsync://cobbler100.athenahealth.com/athena/prod rsync://cobbler100.athenahealth.com/athena/techops rsync://cobbler100.athenahealth.com/athena/scripts rsync://cobbler100.athenahealth.com/athena/intranet rsync://cobbler100.athenahealth.com/athena/athenax /usr/local/athena"
 EOM
 
-echo '172.25.21.207 devhome' >> /etc/hosts
-echo '172.25.21.208 fs22b' >> /etc/hosts
+echo '172.28.21.202 fshome100a' >> /etc/hosts
 
 sync
